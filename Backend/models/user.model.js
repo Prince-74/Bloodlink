e5659d028fef55f0540696057a4e1d1b46450cd9
PRINCE GOYAL <141664327+Prince-74@users.noreply.github.com>
2025-04-24 22:38:25 +0530

    Update user.model.js

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+// updated userschema 
 
 const userSchema = new mongoose.Schema({
     fullname: {
